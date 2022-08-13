3d4234fde92267a1dc3171506d8e435bf2ead2da
vladlen.safaniyuk@gmail.com <wunderking13426451>
2022-08-14 00:42:17 +0300

    Edited Project.

--- src/test/java/BaseUI.java
+++ src/test/java/BaseUI.java
@@ -1,8 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
@@ -12,7 +12,7 @@ public class BaseUI {
     String url = "https://romanceabroad.com/";
     WebDriverWait wait;
 
-    @BeforeMethod
+    @BeforeTest
     public void setUp() {
         driver = new ChromeDriver();
         driver.get(url);
@@ -20,7 +20,7 @@ public class BaseUI {
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         //driver.quit();
 
--- src/test/java/MaineTests.java
+++ src/test/java/MaineTests.java
@@ -9,8 +9,8 @@ public class MaineTests extends BaseUI {
     By registrationButton = By.cssSelector("button[id='show-registration-block']");
     By textFieldEmail = By.cssSelector("input[id='email']");
     By textFieldPassword = By.cssSelector("input[id='password']");
+    By nextButton = By.cssSelector("button[data-action='next-page']");
     By getTextFieldUsername = By.cssSelector("input[name='data[nickname]']");
-    By nextButton = By.xpath("//button[@data-action='next-page'][text()='Next']");
     By Birthdate = By.cssSelector("a[class='btn btn-default dropdown-toggle btn-lg btn-block']");
     By date = By.cssSelector("a[data-value='19']");
     By month = By.cssSelector("a[class='btn btn-default dropdown-toggle btn-lg btn-block']");
@@ -21,7 +21,7 @@ public class MaineTests extends BaseUI {
     By locationone = By.cssSelector("input[name='region_name']");
     By codeone = By.cssSelector("input[id='g-recaptcha-response']");
     By tap = By.cssSelector("input[data-action='validation']");
-    By nextButtonzero = By.xpath("//button[@data-action='update-profile'][text()='Next']");
+    //By nextButtonzero = By.xpath("//button[@data-action='update-profile'][text()='Next']");
     String email = "ukrainemyhome@gmail.com";
     String password = "12345677654321";
     String username = "VladlenSafaniyuk";
@@ -36,7 +36,8 @@ public class MaineTests extends BaseUI {
         driver.findElement(textFieldEmail).sendKeys(email);
         driver.findElement(textFieldPassword).sendKeys(password);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.findElement(nextButton).click();
+        driver.findElements(nextButton).get(1).click();
+        driver.findElements(nextButton).get(1).click();
         driver.findElement(getTextFieldUsername).sendKeys(username);
         driver.findElement(Birthdate).click();
         driver.findElements(date).get(0).click();
@@ -48,28 +49,33 @@ public class MaineTests extends BaseUI {
         driver.findElement(locationone).sendKeys(location);
         driver.findElement(codeone).sendKeys(code);
         driver.findElements(tap).get(4).click();
-        driver.findElement(nextButtonzero).click();
+        //driver.findElement(nextButtonzero).click();
     }
-    
+    By clickPW = By.xpath("//a[@class='nav-link'][text()='PRETTY WOMEN']");
+    By min = By.cssSelector("select[name='age_min']");
+    By YEARS19 = By.cssSelector("option[value='20']");
+    By max = By.cssSelector("select[name='age_max']");
+    By YEARS24 = By.cssSelector("option[value='24']");
+    By NAME = By.cssSelector("select[class='form-control']");
+    By NAME1 = By.cssSelector("option[value='name']");
+    By search = By.xpath("//input[@name='main_search_button']");
+    By listview = By.cssSelector("a[title='List view']");
+
     @Test
     public void mainePageTests2() {
-        driver.findElement(registrationButton).click();
-        driver.findElement(textFieldEmail).sendKeys(email);
-        driver.findElement(textFieldPassword).sendKeys(password);
-        wait.until(ExpectedConditions.elementToBeClickable(nextButton));
-        driver.findElement(nextButton).click();
-        driver.findElement(getTextFieldUsername).sendKeys(username);
-        driver.findElement(Birthdate).click();
-        driver.findElements(date).get(0).click();
-        driver.findElements(month).get(1).click();
-        driver.findElement(monthone).click();
-        driver.findElements(year).get(2).click();
-        driver.findElement(yearone).click();
-        driver.findElement(phoneone).sendKeys(phone);
-        driver.findElement(locationone).sendKeys(location);
-        driver.findElement(codeone).sendKeys(code);
-        driver.findElements(tap).get(4).click();
-        driver.findElement(nextButtonzero).click();
+        driver.findElement(clickPW).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(min).click();
+        driver.findElements(YEARS19).get(0).click();
+        wait.until(ExpectedConditions.elementToBeClickable(max));
+        driver.findElement(max).click();
+        driver.findElements(YEARS24).get(0).click();
+        driver.findElements(NAME).get(2).click();
+        driver.findElement(NAME1).click();
+        driver.findElement(search).click();
+        driver.findElement(listview).click();
+
+
     }
 
 
